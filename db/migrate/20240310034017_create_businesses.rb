cae1ad7c8a5210091c9997cfff760703dc4ed01a
detrix42 <detrix42@gmail.com>
2024-04-28 07:15:06 -0400

    I seem to need to enable the extension 'pgcrypto'.

--- db/migrate/20240310034017_create_businesses.rb
+++ db/migrate/20240310034017_create_businesses.rb
@@ -1,5 +1,7 @@
 class CreateBusinesses < ActiveRecord::Migration[7.1]
+
   def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
     create_table :businesses, id: :uuid do |t|
       t.string :name
       t.string :address1
